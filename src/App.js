a44172c9e4627302d0b8a02da4c017bdda27236c
Dekhil Omran <dekhil22omran@gmail.com>
2022-12-20 07:47:11 +0100

    edit all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1 className="code">dekhil omran</h1>
+      <p>my first web site with react js</p>
+    </section>
   );
 }
 
